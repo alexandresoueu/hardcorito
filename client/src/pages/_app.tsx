6ac6f2aa6292bf700ab1cf4837f565ac75edd89c
c.oliveira.e.silva <calexandredev@gmail.com>
2020-12-11 07:20:26 -0300

    config styled component

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import {} from 'next/app'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Nemo - Boilerplate</title>
+        <link rel="short icon" href="/favicon.ico" />
+        <meta name="description" content="Swimming in the sea..." />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
