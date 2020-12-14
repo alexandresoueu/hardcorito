3d075d86ba298275231708cd00866723c061ac0a
c.oliveira.e.silva <calexandredev@gmail.com>
2020-12-14 08:38:17 -0300

    create first layout with styled component

--- client/src/components/Main/index.test.tsx
+++ client/src/components/Main/index.test.tsx
@@ -3,13 +3,19 @@ import { render, screen } from '@testing-library/react'
 import Main from '.'
 
 describe('MAIN', () => {
-  it('mainzin', () => {
+  it('Should render heading to Main', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /bulbassauro/i })
+      screen.getByRole('heading', { name: /Desenvolvimento em React com Next/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('Should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Bulbassauros</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src='/img/logo.svg'
+      alt='Logo do React Avançado na esquerda da tela'
+    />
+    <S.Title>Desenvolvimento em React com Next</S.Title>
+    <S.Description>criando uma applicação para conhecer o tal do next</S.Description>
+    <S.illustration
+      src='/img/hero-illustration.svg'
+      alt="pessoa sentada na cadeira escrevendo no computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
