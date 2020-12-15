49c8e20eae269a5c1ddf4ead8951ee41d756f91e
c.oliveira.e.silva <calexandredev@gmail.com>
2020-12-15 19:44:19 -0300

    Add new component

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -3,13 +3,15 @@ import * as S from './styles'
 const Main = () => (
   <S.Wrapper>
     <S.Logo
-      src='/img/logo.svg'
-      alt='Logo do React Avançado na esquerda da tela'
+      src="/img/logo.svg"
+      alt="Logo do React Avançado na esquerda da tela"
     />
     <S.Title>Desenvolvimento em React com Next</S.Title>
-    <S.Description>criando uma applicação para conhecer o tal do next</S.Description>
+    <S.Description>
+      criando uma applicação para conhecer o tal do next
+    </S.Description>
     <S.illustration
-      src='/img/hero-illustration.svg'
+      src="/img/hero-illustration.svg"
       alt="pessoa sentada na cadeira escrevendo no computador"
     />
   </S.Wrapper>
